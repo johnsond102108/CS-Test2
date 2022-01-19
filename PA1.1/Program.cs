1b7907c8d49fd0ff3cb2d53bd593ca0885da0e6f
Davis Johnson <davbjohn@ut.utm.edu>
2022-01-18 20:03:59 -1000

    Test

--- PA1.1/Program.cs
+++ PA1.1/Program.cs
@@ -7,6 +7,18 @@ namespace PA1._1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Program p = new Program();
+            p.sayGoodbye();
+            Cat c = new Cat();
+            Cat j = new Cat("Philbert");
+            c.printCat();
+            j.printCat();
+            Console.ReadKey();
+        }
+
+        void sayGoodbye()
+        {
+            Console.WriteLine("Goodbye world!");
         }
     }
 }
