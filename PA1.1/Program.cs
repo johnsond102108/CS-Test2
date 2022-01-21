ca3609a4768ec6919a26b0183834b51a850a42c5
Davis Johnson <davbjohn@ut.utm.edu>
2022-01-21 00:12:42 -0600

    Guessing game fully implemented.

--- PA1.1/Program.cs
+++ PA1.1/Program.cs
@@ -4,19 +4,42 @@ namespace PA1._1
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("I am thinking of a number between 0 and 100.");
+            Console.WriteLine("You have 5 attempts. Number's between 0 and 100.");
             Random rd = new Random();
-            int numRoll = rd.Next(0, 100);
+            int numRoll = rd.Next(0, 101);
 
             int numGuess;
             int lives = 5;
             while (lives != 0)
             {
-                Console.WriteLine("Please guess a number.");
+                Console.WriteLine("Please enter a number.");
+                numGuess = Convert.ToInt32(Console.ReadLine());
                 
+                if (numGuess == numRoll)
+                {
+                    Console.WriteLine("You had " + lives + "attempts remaining. Put in a little effort next time, yeah?");
+                    break;
+                }
+                else if(lives > 1)
+                {
+                    lives -= 1;
+                    if(numGuess > numRoll)
+                    {
+                        Console.WriteLine("Have you tried lowering your expectations?");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You can't reach the stars, but you can go a little higher.");
+                    }
+                }else
+                {
+                    Console.WriteLine("You failed. Have you tried typing an answer?");
+                    break;
+                }
             }
+            return 0;
         }
     }
 }
